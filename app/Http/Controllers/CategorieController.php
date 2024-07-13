535b42de7f50b3e095dc38fe486e73e98a9d560b
valerierandria07 <137295953+valerierandria07@users.noreply.github.com>
2024-07-13 21:53:54 +0300

    Create CategorieController.php

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Categorie;
+use Illuminate\Http\Request;
+
+class CategorieController extends Controller
+{
+    public function selectCategorie()
+    {
+       $result=Categorie::all();
+       return view('lien', ['results' => $result]);
+    }
+}
